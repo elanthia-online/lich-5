84c0320e194950117aba28b558533ee944ade8a4
Charlie Sanders <sanderscharlie@gmail.com>
2022-12-29 00:04:50 -0600

    Adds helper for getting Bounty::Task through LNet

--- lib/bounty.rb
+++ lib/bounty.rb
@@ -12,6 +12,20 @@ class Bounty
     current
   end
 
+  def self.lnet(person)
+    if target_info = LNet.get_data(person.dup, 'bounty')
+      Task.new(Parser.parse(target_info))
+    else
+      if target_info == false
+        text = "No one on LNet with a name like #{person}"
+      else
+        text = "Empty response from LNet for bounty from #{person}\n"
+      end
+      Lich::Messaging.msg("warn", text)
+      nil
+    end
+  end
+
   # Delegate class methods to a new instance of the current bounty task
   [:status, :type, :requirements, :town, :any?, :none?, :done?].each do |attr|
     self.class.instance_eval do
