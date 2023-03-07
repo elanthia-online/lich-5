031cf796dfcf12b32c665db3ecaa9cbaaf1cc6a1
Benjamin Clos <benjamin.clos@gmail.com>
2023-03-07 11:47:36 -0700

    feat: add hmr module;

---
+++ lib/hmr.rb
@@ -0,0 +1,22 @@
+## hot module reloading
+module HMR
+  def self.clear_cache
+    Gem.clear_paths
+  end
+
+  def self.loaded
+    $LOADED_FEATURES.select {|path| path.end_with?(".rb") }
+  end
+
+  def self.reload(pattern)
+    self.loaded.grep(pattern).each {|file|
+      begin
+        load(file)
+        _respond "<b>[lich.hmr] reloaded %s</b>" % file
+      rescue => exception
+        respond exception
+        respond exception.backtrace.join("\n")
+      end
+    }
+  end
+end
