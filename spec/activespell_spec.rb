b9fe28709a740a4962bad61632771182f2efb328
OSXLich-Doug <doug@play.net>
2023-03-16 18:38:07 -0400

    bugfix:add Time.parse to hash in spec file, and rubocop run;

--- spec/activespell_spec.rb
+++ spec/activespell_spec.rb
@@ -1,9 +1,13 @@
 require 'infomon/activespells'
 
+# actual format of XMLData.active_spells is a hash represented as
+# "Spell.name"=>String where string is a Time obj converted to string.
+# To test capture we will use the following format in the response
+# "Spell.name"=>Time.parse("String") which allows the hash to be instantiated
+
 module XMLData
   def self.active_spells
-
-    testbed = {"Bravery"=>2023-03-16 16:23:01.205027 -0400, "Heroism"=>2023-03-16 16:23:01.205062 -0400, "Elemental Defense I"=>2023-03-16 18:35:20.205097 -0400, "Elemental Defense II"=>2023-03-16 18:35:20.205133 -0400, "Elemental Defense III"=>2023-03-16 18:35:20.205167 -0400, "Elemental Targeting"=>2023-03-16 18:35:20.205202 -0400, "Elemental Barrier"=>2023-03-16 18:35:20.205238 -0400, "Thurfel's Ward"=>2023-03-16 18:35:20.205272 -0400, "Elemental Deflection"=>2023-03-16 18:35:20.205304 -0400, "Elemental Bias"=>2023-03-16 18:35:20.205339 -0400, "Strength"=>2023-03-16 18:35:20.205373 -0400, "Elemental Focus"=>2023-03-16 18:35:20.205405 -0400, "Mage Armor - Fire"=>2023-03-16 18:35:20.205442 -0400, "Haste"=>2023-03-16 18:35:20.205472 -0400, "Temporal Reversion"=>2023-03-16 18:35:20.205507 -0400, "Prismatic Guard"=>2023-03-16 18:35:20.205541 -0400, "Mass Blur"=>2023-03-16 18:35:20.205575 -0400, "Melgorehn's Aura"=>2023-03-16 18:35:20.205606 -0400}
+    testbed = { "Bravery" => Time.parse("2023-03-16 16:23:01.205027 -0400"), "Heroism" => Time.parse("2023-03-16 16:23:01.205062 -0400"), "Elemental Defense I" => Time.parse("2023-03-16 18:35:20.205097 -0400"), "Elemental Defense II" => Time.parse("2023-03-16 18:35:20.205133 -0400"), "Elemental Defense III" => Time.parse("2023-03-16 18:35:20.205167 -0400"), "Elemental Targeting" => Time.parse("2023-03-16 18:35:20.205202 -0400"), "Elemental Barrier" => Time.parse("2023-03-16 18:35:20.205238 -0400"), "Thurfel's Ward" => Time.parse("2023-03-16 18:35:20.205272 -0400"), "Elemental Deflection" => Time.parse("2023-03-16 18:35:20.205304 -0400"), "Elemental Bias" => Time.parse("2023-03-16 18:35:20.205339 -0400"), "Strength" => Time.parse("2023-03-16 18:35:20.205373 -0400"), "Elemental Focus" => Time.parse("2023-03-16 18:35:20.205405 -0400"), "Mage Armor - Fire" => Time.parse("2023-03-16 18:35:20.205442 -0400"), "Haste" => Time.parse("2023-03-16 18:35:20.205472 -0400"), "Temporal Reversion" => Time.parse("2023-03-16 18:35:20.205507 -0400"), "Prismatic Guard" => Time.parse("2023-03-16 18:35:20.205541 -0400"), "Mass Blur" => Time.parse("2023-03-16 18:35:20.205575 -0400"), "Melgorehn's Aura" => Time.parse("2023-03-16 18:35:20.205606 -0400") }
 
     return testbed
   end
