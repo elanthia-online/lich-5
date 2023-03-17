6639b04e58d74be5f5550d947f7769a7eaa18002
OSXLich-Doug <doug@play.net>
2023-03-17 19:10:33 -0400

    chore:restructure to remove instance variables;

--- spec/activespell_spec.rb
+++ spec/activespell_spec.rb
@@ -1,33 +1,29 @@
-require 'infomon/activespell'
-
 # actual format of XMLData.active_spells is a hash represented as
 # "Spell.name"=>String where string is a Time obj converted to string.
 # To test capture we will use the following format in the response
-# "Spell.name"=>"String" which allows the hash to be instantiated
+# "Spell.name"=>"String") which allows the hash to be instantiated
+
+require 'infomon/activespells'
 
 module XMLData
-  def self.active_spells
-    testbed = { "Spirit Warding I" => "2023-03-16 20:53:54.138611 -0400", "Iron Skin" => "2023-03-16 21:01:58.138801 -0400", "Foresight" => "2023-03-16 21:02:26.139419 -0400", "Mindward" => "2023-03-16 21:02:32.139583 -0400", "Dragonclaw" => "2023-03-16 21:02:38.139756 -0400", "Rolling Krynch Stance" => "2033-03-13 20:33:39.139986 -0400" }
-
-    return testbed
-  end
-
   def self.process_spell_durations=(val)
     val
   end
 end
 
-describe ActiveSpell do
+describe ActiveSpells do
   context "updates spell information" do
     it "queries XMLData" do
+      testbed = {"Bravery"=>"2023-03-16 16:23:01.205027 -0400", "Heroism"=>"2023-03-16 16:23:01.205062 -0400", "Elemental Defense I"=>"2023-03-16 18:35:20.205097 -0400", "Elemental Defense II"=>"2023-03-16 18:35:20.205133 -0400", "Elemental Defense III"=>"2023-03-16 18:35:20.205167 -0400", "Elemental Targeting"=>"2023-03-16 18:35:20.205202 -0400", "Elemental Barrier"=>"2023-03-16 18:35:20.205238 -0400", "Thurfel's Ward"=>"2023-03-16 18:35:20.205272 -0400", "Elemental Deflection"=>"2023-03-16 18:35:20.205304 -0400", "Elemental Bias"=>"2023-03-16 18:35:20.205339 -0400", "Strength"=>"2023-03-16 18:35:20.205373 -0400", "Elemental Focus"=>"2023-03-16 18:35:20.205405 -0400", "Mage Armor - Fire"=>"2023-03-16 18:35:20.205442 -0400", "Haste"=>"2023-03-16 18:35:20.205472 -0400", "Temporal Reversion"=>"2023-03-16 18:35:20.205507 -0400", "Prismatic Guard"=>"2023-03-16 18:35:20.205541 -0400", "Mass Blur"=>"2023-03-16 18:35:20.205575 -0400", "Melgorehn's Aura"=>"2023-03-16 18:35:20.205606 -0400"}
+
       XMLData.process_spell_durations = true
-      ActiveSpell.get_spell_info
+      update_spell_names, update_spell_durations = ActiveSpell.get_spell_info(testbed)
 
-      expect(ActiveSpell.instance_variable_get("@update_spell_names")).to eql(["Spirit Warding I", "Iron Skin", "Foresight", "Mindward", "Dragonclaw", "Rolling Krynch Stance"])
+      expect(update_spell_names).to eq(["Bravery", "Heroism", "Elemental Defense I", "Elemental Defense II", "Elemental Defense III", "Elemental Targeting", "Elemental Barrier", "Thurfel's Ward", "Elemental Deflection", "Elemental Bias", "Strength", "Elemental Focus", "Mage Armor", "Haste", "Temporal Reversion", "Prismatic Guard", "Mass Blur", "Melgorehn's Aura"])
 
-      expect(ActiveSpell.instance_variable_get("@update_spell_durations").keys[0]).to eql(%[Spirit Warding I])
-      expect(ActiveSpell.instance_variable_get("@update_spell_durations").values[2]).to eql(%[2023-03-16 21:02:26.139419 -0400])
-      expect(ActiveSpell.instance_variable_get("@update_spell_durations").keys[5]).to eql(%[Rolling Krynch Stance])
+      expect(update_spell_durations.keys[0]).to eq(%[Bravery])
+      expect(update_spell_durations.values[2]).to eq(%[2023-03-16 18:35:20.205097 -0400])
+      expect(update_spell_durations.keys[17]).to eq(%[Mage Armor])
     end
   end
 end
