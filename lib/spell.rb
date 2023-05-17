20ba866e979d244b4180efe6aede3e32f03d1f2c
OSXLich-Doug <doug@play.net>
2023-05-17 13:18:15 -0500

    change to File.join method

--- lib/spell.rb
+++ lib/spell.rb
@@ -169,8 +169,7 @@ module Games
 
       def Spell.load(filename = nil)
         if filename.nil?
-          # filename = "#{DATA_DIR}/spell-list.xml" #switching to effects-list.xml for EOL ownership
-          filename = "#{DATA_DIR}/effect-list.xml"
+           filename = File.join(DATA_DIR, 'effect-list.xml')
         end
         # script = Script.current #rubocop useless assignment to variable - script
         Script.current
