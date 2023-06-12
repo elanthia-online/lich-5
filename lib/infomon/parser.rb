a05375db2791c67fb9c598281dc2b9d392ee599f
OSXLich-Doug <doug@play.net>
2023-06-12 11:33:21 -0500

    modest rubocop suggested correction

--- lib/infomon/parser.rb
+++ lib/infomon/parser.rb
@@ -135,8 +135,7 @@ module Infomon
         when Pattern::Levelup
           match = Regexp.last_match
           Infomon.upsert_batch([['stat.%s' % match[:stat], match[:value].to_i],
-                               ['stat.%s_bonus' % match[:stat], match[:bonus].to_i]]
-          )
+                               ['stat.%s_bonus' % match[:stat], match[:bonus].to_i]])
           :ok
         when Pattern::SpellsSolo
           match = Regexp.last_match
