e54e80255b59ff5f007458710b8f10958107aef4
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-06-12 20:27:10 -0400

    Update infomon_spec.rb
    attempt to fix spectest

--- spec/infomon_spec.rb
+++ spec/infomon_spec.rb
@@ -26,9 +26,9 @@ describe Infomon, ".setup!" do
     it "upserts a new key/value pair" do
       k = "stats.influence"
       # handles when value doesn't exist
-      Infomon.set(k, 30)
+      pp Infomon.set(k, 30)
       expect(Infomon.get(k)).to eq(30)
-      Infomon.set(k, 40)
+      pp Infomon.set(k, 40)
       # handles upsert on already existing values
       expect(Infomon.get(k)).to eq(40)
     end
