c02b5530d105d56c8ed1ad886a7ce24bef2b300e
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-06-13 17:55:35 -0400

    pp the output of additional tests

--- spec/infomon_spec.rb
+++ spec/infomon_spec.rb
@@ -26,9 +26,9 @@ describe Infomon, ".setup!" do
     it "upserts a new key/value pair" do
       k = "stats.influence"
       # handles when value doesn't exist
-      Infomon.set(k, 30)
+      pp Infomon.set(k, 30)
       expect(Infomon.get(k)).to eq(30)
-      Infomon.set(k, 40)
+      pp Infomon.set(k, 40)
       # handles upsert on already existing values
       expect(Infomon.get(k)).to eq(40)
     end
@@ -43,13 +43,13 @@ describe Infomon::Parser, ".parse" do
 
   context "citizenship" do
     it "handles citizenship in a town" do
-      Infomon::Parser.parse %[You currently have full citizenship in Wehnimer's Landing.]
+      pp Infomon::Parser.parse %[You currently have full citizenship in Wehnimer's Landing.]
       citizenship = Infomon.get("citizenship")
       expect(citizenship).to eq(%[Wehnimer's Landing])
     end
 
     it "handles no citizenship" do
-      Infomon::Parser.parse %[You don't seem to have citizenship.]
+      pp Infomon::Parser.parse %[You don't seem to have citizenship.]
       expect(Infomon.get("citizenship")).to eq("None")
     end
   end
