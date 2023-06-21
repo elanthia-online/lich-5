d599910ef9460f7394e0dc93fde3b800931837cb
OSXLich-Doug <doug@play.net>
2023-06-21 15:52:46 -0500

    correcting rubocop error in current infomon_spec

--- spec/infomon_spec.rb
+++ spec/infomon_spec.rb
@@ -387,7 +387,7 @@ Feat
       100.times do
         Infomon.reset!
         Infomon.cache.flush!
-        
+
         expect(Infomon.get(k)).to be_nil
         Infomon.set(k, 42)
         expect(Infomon.get(k)).to eq(42)
