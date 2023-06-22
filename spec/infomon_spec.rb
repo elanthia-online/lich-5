bd08376c718831509ad6e18c854b7629a2a3a73c
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-06-22 12:19:18 -0400

    Update infomon_spec.rb

--- spec/infomon_spec.rb
+++ spec/infomon_spec.rb
@@ -21,7 +21,7 @@ require "spell"
 download = URI.open('https://raw.githubusercontent.com/elanthia-online/scripts/master/scripts/effect-list.xml').read
 FileUtils.mkdir_p('/home/runner/work/lich-5/lich-5/data')
 File.write('/home/runner/work/lich-5/lich-5/data/effect-list.xml', download)
-echo Dir.children('/home/runner/work/lich-5/lich-5/data/')
+pp "Data Files: #{Dir.children('/home/runner/work/lich-5/lich-5/data/')}"
 Games::Gemstone::Spell.load('/home/runner/work/lich-5/lich-5/data/effect-list.xml')
 
 require "infomon/infomon"
