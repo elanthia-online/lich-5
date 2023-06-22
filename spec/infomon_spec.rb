a117298345684f4ebdd6ace3e979c223e1dd1322
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-06-22 12:27:12 -0400

    Update infomon_spec.rb
    add NilClass overloading.

--- spec/infomon_spec.rb
+++ spec/infomon_spec.rb
@@ -14,6 +14,36 @@ module Lich
   end
 end
 
+class NilClass
+  def dup
+    nil
+  end
+
+  def method_missing(*args)
+    nil
+  end
+
+  def split(*val)
+    Array.new
+  end
+
+  def to_s
+    ""
+  end
+
+  def strip
+    ""
+  end
+
+  def +(val)
+    val
+  end
+
+  def closed?
+    true
+  end
+end
+
 require 'rexml/document'
 require 'rexml/streamlistener'
 require 'open-uri'
