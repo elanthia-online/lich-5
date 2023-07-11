773ee795390b1412095c793cf99eea0d60333f7a
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-07-11 15:35:41 -0400

    Update resource.rb
    Add `self.type`

--- lib/attributes/resource.rb
+++ lib/attributes/resource.rb
@@ -11,6 +11,10 @@ module Resource
     Infomon.get('resource.suffused')
   end
 
+  def self.type
+    Infomon.get('resource.type')
+  end
+
   def self.check(quiet = false)
     Lich::Util.issue_command('resource', /^Health: \d+\/(?:<pushBold\/>)?\d+\s+Mana: \d+\/(?:<pushBold\/>)?\d+\s+Stamina: \d+\/(?:<pushBold\/>)?\d+\s+Spirit: \d+\/(?:<pushBold\/>)?\d+/, /<prompt/, true, 5, true, true, quiet)
     return [self.weekly, self.total, self.suffused]
