d983283ac3f076eef84191d0dd160d8d218d544b
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-07-11 14:26:25 -0400

    Update resource.rb
    remove debug output `respond` of Lich::Util.issue_command

--- lib/attributes/resource.rb
+++ lib/attributes/resource.rb
@@ -12,7 +12,7 @@ module Resource
   end
 
   def self.check(quiet = false)
-    respond Lich::Util.issue_command('resource', /^Health: \d+\/(?:<pushBold\/>)?\d+\s+Mana: \d+\/(?:<pushBold\/>)?\d+\s+Stamina: \d+\/(?:<pushBold\/>)?\d+\s+Spirit: \d+\/(?:<pushBold\/>)?\d+/, /<prompt/, true, 5, true, true, quiet)
+    Lich::Util.issue_command('resource', /^Health: \d+\/(?:<pushBold\/>)?\d+\s+Mana: \d+\/(?:<pushBold\/>)?\d+\s+Stamina: \d+\/(?:<pushBold\/>)?\d+\s+Spirit: \d+\/(?:<pushBold\/>)?\d+/, /<prompt/, true, 5, true, true, quiet)
     return [self.weekly, self.total, self.suffused]
   end
 end
