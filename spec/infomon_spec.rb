bf07deefde89f2da576ad1dd1e768a00b6936f7c
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-07-14 10:06:13 -0400

    Update infomon_spec.rb
    update resource to resources

--- spec/infomon_spec.rb
+++ spec/infomon_spec.rb
@@ -259,14 +259,14 @@ describe Infomon::Parser, ".parse" do
       Resource
       output.split("\n").map { |line| Infomon::Parser.parse(line) }
 
-      expect(Infomon.get("resource.weekly")).to eq(34000)
-      expect(Infomon.get("resource.total")).to eq(78507)
-      expect(Infomon.get("resource.suffused")).to eq(1678)
-      expect(Infomon.get("resource.type")).to eq("Essence")
-      expect(Resource.weekly).to eq(34000)
-      expect(Resource.total).to eq(78507)
-      expect(Resource.suffused).to eq(1678)
-      expect(Resource.type).to eq("Essence")
+      expect(Infomon.get("resources.weekly")).to eq(34000)
+      expect(Infomon.get("resources.total")).to eq(78507)
+      expect(Infomon.get("resources.suffused")).to eq(1678)
+      expect(Infomon.get("resources.type")).to eq("Essence")
+      expect(Lich::Resources.weekly).to eq(34000)
+      expect(Lich::Resources.total).to eq(78507)
+      expect(Lich::Resources.suffused).to eq(1678)
+      expect(Lich::Resources.type).to eq("Essence")
     end
   end
 
