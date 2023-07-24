0b1fe2b53cf557b64ce42366473190374919ff85
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-07-24 12:38:22 -0400

    [resources.rb] bugfix to account for pushbold/popbold fully

--- lib/attributes/resources.rb
+++ lib/attributes/resources.rb
@@ -17,7 +17,7 @@ module Lich
     end
 
     def self.check(quiet = false)
-      Lich::Util.issue_command('resource', /^Health: \d+\/(?:<pushBold\/>)?\d+\s+Mana: \d+\/(?:<pushBold\/>)?\d+\s+Stamina: \d+\/(?:<pushBold\/>)?\d+\s+Spirit: \d+\/(?:<pushBold\/>)?\d+/, /<prompt/, silent: true, quiet: quiet)
+      Lich::Util.issue_command('resource', /^Health: \d+\/(?:<pushBold\/>)?\d+(?:<popBold\/>)?\s+Mana: \d+\/(?:<pushBold\/>)?\d+(?:<popBold\/>)?\s+Stamina: \d+\/(?:<pushBold\/>)?\d+(?:<popBold\/>)?\s+Spirit: \d+\/(?:<pushBold\/>)?\d+/, /<prompt/, silent: true, quiet: quiet)
       return [self.weekly, self.total, self.suffused]
     end
   end
