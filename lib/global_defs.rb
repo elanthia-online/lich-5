0a4117766769987f7addf72179825392ad60c972
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2023-10-03 15:07:48 -0400

    [global_defs.rb] add various help output
    Add `;help` output for Infomon.sync, and setting Lich/Real room IDs in room title.

--- lib/global_defs.rb
+++ lib/global_defs.rb
@@ -2181,12 +2181,14 @@ def do_client(client_string)
       respond("--- Lich: toggle #{toggle_var} set #{set_state}") if did_something
       did_something = false
       nil
-    elsif cmd =~ /^(?:lich5-update|l5u)\s+(.*)/i
-      update_parameter = $1.dup
-      Lich::Util::Update.request("#{update_parameter}")
     elsif cmd =~ /^hmr\s+(?<pattern>.*)/i
       require "lib/hmr"
       HMR.reload %r{#{Regexp.last_match[:pattern]}}
+    elsif XMLData.game =~ /^GS/ && cmd =~ /^infomon sync)/i
+      Infomon.sync
+    elsif cmd =~ /^(?:lich5-update|l5u)\s+(.*)/i
+      update_parameter = $1.dup
+      Lich::Util::Update.request("#{update_parameter}")
     elsif cmd =~ /^(?:lich5-update|l5u)/i
       Lich::Util::Update.request("--help")
     elsif cmd =~ /^banks$/ && XMLData.game =~ /^GS/
@@ -2243,6 +2245,7 @@ def do_client(client_string)
       respond "   #{$clean_lich_char}set <variable> [on|off]   set a global toggle variable on or off"
       respond "   #{$clean_lich_char}lich5-update --<command>  Lich5 ecosystem management "
       respond "                                                see #{$clean_lich_char}lich5-update --help"
+      respond "   #{$clean_lich_char}infomon sync              sends all the various commands to resync character data for infomon" if XMLData.game =~ /^GS/
       respond
       respond 'If you liked this help message, you might also enjoy:'
       respond "   #{$clean_lich_char}lnet help" if defined?(LNet)
