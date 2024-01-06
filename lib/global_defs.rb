f4f6391bf8525ebf10a284e127e3186c2aff15bc
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2024-01-06 00:34:54 -0500

    Update global_defs.rb
    add log msg for depreciated method

--- lib/global_defs.rb
+++ lib/global_defs.rb
@@ -920,6 +920,7 @@ def percentconcentration(num = nil)
 end
 
 def checkstance(*args)
+  Lich.log("Depreciated method used - checkstance - Script: #{Script.current.name} - Change to use Char.stance instead!")
   Char.stance(*args)
 end
 
