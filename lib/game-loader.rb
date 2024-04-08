71cce3cfd2c48d7d202ffd0ebbdd5598342c3db7
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2024-04-08 10:31:41 -0400

    move to be after spell.rb loading

--- lib/game-loader.rb
+++ lib/game-loader.rb
@@ -3,6 +3,7 @@ module GameLoader
   def self.gemstone
     require 'lib/map/map_gs.rb'
     require 'lib/spell'
+    require 'lib/effects'
     require 'lib/bounty'
     require 'lib/claim'
     require 'lib/infomon/infomon'
@@ -21,7 +22,6 @@ module GameLoader
     require 'lib/psms'
     require 'lib/attributes/char'
     require 'lib/infomon/currency'
-    require 'lib/effects'
   end
 
   def self.dragon_realms
