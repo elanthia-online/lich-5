1dee2f79b4611297258649ee1b0a18bb96df3356
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2024-05-02 12:08:32 -0400

    limit log spam by default

--- lib/lich.rb
+++ lib/lich.rb
@@ -2,6 +2,7 @@ module Lich
   @@hosts_file           = nil
   @@lich_db              = nil
   @@last_warn_deprecated = 0
+  @@deprecated_log       = []
 
   # settings
   @@display_lichid       = nil # boolean
@@ -58,12 +59,13 @@ module Lich
     $stderr.puts "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}: #{msg}"
   end
 
-  def Lich.deprecated(old_object = '', new_object = '', script_location = "#{Script.current.name || 'unknown'}", debug_log: true, fe_log: false)
-    msg = "deprecated call to #{old_object} used"
-    msg += " in #{script_location}"
-    msg += ". Please change to #{new_object} instead!"
+  def Lich.deprecated(old_object = '', new_object = '', script_location = "#{Script.current.name || 'unknown'}", debug_log: true, fe_log: false, limit_log: true)
+    return if limit_log && @@deprecated_log.include?("#{script_location}-#{old_object}-#{new_object}")
+    msg = "Deprecated call to #{old_object} used in #{script_location}. Please change to #{new_object} instead!"
     Lich.log(msg) if debug_log
     _respond Lich::Messaging.monsterbold(msg) if fe_log
+    @@deprecated_log.push("#{script_location}-#{old_object}-#{new_object}") unless @@deprecated_log.include?("#{script_location}-#{old_object}-#{new_object}")
+    end
   end
 
   def Lich.msgbox(args)
