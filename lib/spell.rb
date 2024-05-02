4a172cbc6ae2e06451afc4e81396440a0318dabb
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2024-05-02 11:46:33 -0400

    Update spell.rb

--- lib/spell.rb
+++ lib/spell.rb
@@ -571,7 +571,7 @@ module Games
           ## convert Spell[9699].active? to Effects::Debuffs test (if Debuffs is where it shows)
           if (Feat.known?(:mental_acuity) and self.num.between?(1201, 1220)) and (Spell[9699].active? or not Char.stamina >= (self.mana_cost(options) * 2) or Effects::Debuffs.active?("Overexerted"))
             false
-          elsif (!(Feat.known?(:mental_acuity) and self.num.between?(1201, 1220))) and (!(Char.mana >= self.mana_cost(options)) or (Spell[515].active? and !(Char.mana >= self.mana_cost(options)) + [self.mana_cost(release_options) / 4, 1].max)))
+          elsif (!(Feat.known?(:mental_acuity) and self.num.between?(1201, 1220))) and (!(Char.mana >= self.mana_cost(options)) or (Spell[515].active? and !(Char.mana >= self.mana_cost(options)) + [self.mana_cost(release_options) / 4, 1].max))
             false
           else
             true
