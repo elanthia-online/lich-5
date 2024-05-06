7c8845b4169540dcaa7ec887e63c896242c356bb
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2024-05-06 10:39:22 -0400

    [group.rb] other various fixes
    Fix to set Group.leader appropriately on GROUP output

--- lib/character/group.rb
+++ lib/character/group.rb
@@ -239,9 +239,9 @@ module Games
 
           people = exist(line)
 
-          if line.include?("is following you")
+          if line.include?("You are leading")
             Group.leader = :self
-          elsif line.include?("is the leader of your group")
+          elsif line.include?("who is leading")
             Group.leader = people.first
           end
 
