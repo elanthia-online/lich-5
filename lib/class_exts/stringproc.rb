23c957618dd71ef7b1abec811db9c490ad2e95a9
OSXLich-Doug <doug@play.net>
2024-06-13 16:30:48 -0500

    [all][chore] Break out classes and extensions to lib

---
+++ lib/class_exts/stringproc.rb
@@ -0,0 +1,32 @@
+# Carve out from lich.rbw
+# extension to StringProc class 2024-06-13
+
+class StringProc
+  def initialize(string)
+    @string = string
+  end
+
+  def kind_of?(type)
+    Proc.new {}.kind_of? type
+  end
+
+  def class
+    Proc
+  end
+
+  def call(*_a)
+    proc { eval(@string) }.call
+  end
+
+  def _dump(_d = nil)
+    @string
+  end
+
+  def inspect
+    "StringProc.new(#{@string.inspect})"
+  end
+
+  def to_json(*args)
+    ";e #{_dump}".to_json(args)
+  end
+end
