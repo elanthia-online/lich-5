6be859470df007d3ae5aa3e80a7c8b5c170ed5c8
OSXLich-Doug <doug@play.net>
2024-08-06 14:07:37 -0500

    [gs][critranks.rb] remove hardcode path separator

--- lib/critranks.rb
+++ lib/critranks.rb
@@ -19,7 +19,7 @@ module Games
 
       def self.init
         return unless @critical_table.empty?
-        Dir.glob("#{File.join(LIB_DIR, "crit_tables")}/*critical_table.rb").each do |file|
+        Dir.glob("#{File.join(LIB_DIR, "crit_tables", "*critical_table.rb")}").each do |file|
           require file
         end
         create_indices
