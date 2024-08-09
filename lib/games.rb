9f95200772119c54b8f02ba3dbb361a516cf806f
OSXLich-Doug <doug@play.net>
2024-08-09 15:05:16 -0500

    [all][games.rb]Bugfix - correct for missing room info in some cases
    This fixes a bug wherein an edgecase could cause a CLI user not to be able to use the `--start-scripts` feature.

--- lib/games.rb
+++ lib/games.rb
@@ -204,7 +204,7 @@ module Games
                   @@infomon_loaded = true
                 end
 
-                if @@autostarted and !@@cli_scripts and $_SERVERSTRING_ =~ /roomDesc/
+                if @@autostarted and !@@cli_scripts and defined? XMLData.game
                   if (arg = ARGV.find { |a| a =~ /^\-\-start\-scripts=/ })
                     for script_name in arg.sub('--start-scripts=', '').split(',')
                       Script.start(script_name)
