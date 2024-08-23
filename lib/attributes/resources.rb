66bec7a274d6d2dfbaa83c20cf19e08c4e6c6418
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2024-08-23 11:49:45 -0400

    Update resources.rb for Voln Favor

--- lib/attributes/resources.rb
+++ lib/attributes/resources.rb
@@ -16,6 +16,10 @@ module Lich
       Infomon.get('resources.type')
     end
 
+    def self.voln_favor
+      Infomon.get('resources.voln_favor')
+    end
+
     def self.check(quiet = false)
       Lich::Util.issue_command('resource', /^Health: \d+\/(?:<pushBold\/>)?\d+(?:<popBold\/>)?\s+Mana: \d+\/(?:<pushBold\/>)?\d+(?:<popBold\/>)?\s+Stamina: \d+\/(?:<pushBold\/>)?\d+(?:<popBold\/>)?\s+Spirit: \d+\/(?:<pushBold\/>)?\d+/, /<prompt/, silent: true, quiet: quiet)
       return [self.weekly, self.total, self.suffused]
