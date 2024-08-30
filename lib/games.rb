a22eef6330f17f8ee6583e1ba0cdb8e7905f4d9c
OSXLich-Doug <doug@play.net>
2024-08-30 14:20:05 -0400

    'cause pico seconds matter dammit
    Tysong recommends checking 1 var instead of 2 - implemented

--- lib/games.rb
+++ lib/games.rb
@@ -204,7 +204,7 @@ module Games
                   @@infomon_loaded = true
                 end
 
-                if @@autostarted && !@@cli_scripts && !XMLData.name.empty?
+                if !@@cli_scripts && @@autostarted && !XMLData.name.empty?
                   if (arg = ARGV.find { |a| a =~ /^\-\-start\-scripts=/ })
                     for script_name in arg.sub('--start-scripts=', '').split(',')
                       Script.start(script_name)
