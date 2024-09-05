add5153f65f5d81eb380622bafe6f4b6e47ab367
snperry85 <52931466+snperry85@users.noreply.github.com>
2024-09-05 13:28:36 -0500

    Update grapple_critical_table.rb

--- lib/crit_tables/grapple_critical_table.rb
+++ lib/crit_tables/grapple_critical_table.rb
@@ -1432,7 +1432,7 @@ module Games
                                   :wound_rank      => 1,
                                   :secondary_wound => nil,
                                   :regex           =>
-                                                      /(?:.*?'s l|L)eft hand is wrenched hard as (?:she|he|it|opponent) is thrown to the ground./ },
+                                                      /(?:.*?'s l|L)eft hand (?:is )?wrenched hard as (?:she|he|it|opponent) is thrown to the ground./ },
                            8 =>
                                 { :type            => "Grapple",
                                   :location        => "Left hand",
