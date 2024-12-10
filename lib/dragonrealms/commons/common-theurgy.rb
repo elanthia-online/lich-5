8575501fea2a16d1fcbc7749a3c7b471385e807a
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2024-12-10 10:17:18 -0500

    rubocop cleanup

--- lib/dragonrealms/commons/common-theurgy.rb
+++ lib/dragonrealms/commons/common-theurgy.rb
@@ -107,9 +107,10 @@ module DRCTH
 
   def quick_bless_item(item_name)
     # use dummy settings object since this isn't complex enough for camb, etc.
-    DRCA.cast_spell({
-                      'abbrev' => 'bless', 'mana' => 1, 'prep_time' => 2, 'cast' => "cast my #{item_name}"
-                    }, {})
+    DRCA.cast_spell(
+      {'abbrev' => 'bless', 'mana' => 1, 'prep_time' => 2, 'cast' => "cast my #{item_name}"},
+      {}
+    )
   end
 
   def empty_cleric_hands(theurgy_supply_container)
