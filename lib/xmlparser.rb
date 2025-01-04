6204211c92bbd63552e240a6ff40e13abecfe880
Mahtra <93822896+MahtraDR@users.noreply.github.com>
2025-01-04 18:49:27 +1300

    Add version info

--- lib/xmlparser.rb
+++ lib/xmlparser.rb
@@ -6,9 +6,11 @@ xmlparser.rb: Core lich file that defines the data extracted from SIMU's XML.
     game: Gemstone
     tags: CORE, spells
     required: Lich > 5.7
-    version: 1.3.3
+    version: 1.3.4
 
   changelog:
+    v1.3.4 (2025-01-04)
+      fEATURE: Add support for room IDs in DR
     v1.3.3 (2024-10-31)
       Feature: Add DR Active Spells to XMLData
     v1.3.2 (2024-10-17)
