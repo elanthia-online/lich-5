4193ea57a71a496f9f9ca48cc16b63ecafec97f2
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-01-12 13:00:49 -0500

    remove double covert_arts_charges def

--- lib/attributes/resources.rb
+++ lib/attributes/resources.rb
@@ -24,10 +24,6 @@ module Lich
       Infomon.get('resources.covert_arts_charges')
     end
 
-    def self.covert_arts_charges
-      Infomon.get('resources.covert_arts_charges')
-    end
-
     def self.check(quiet = false)
       Lich::Util.issue_command('resource', /^Health: \d+\/(?:<pushBold\/>)?\d+(?:<popBold\/>)?\s+Mana: \d+\/(?:<pushBold\/>)?\d+(?:<popBold\/>)?\s+Stamina: \d+\/(?:<pushBold\/>)?\d+(?:<popBold\/>)?\s+Spirit: \d+\/(?:<pushBold\/>)?\d+/, /<prompt/, silent: true, quiet: quiet)
       return [self.weekly, self.total, self.suffused]
