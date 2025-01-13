b99c64bb3bd6e57929f7a392bd2ce6f161f68684
OSXLich-Doug <doug@play.net>
2025-01-13 16:23:56 -0600

    back to the future ][

--- spec/psms_spec.rb
+++ spec/psms_spec.rb
@@ -37,8 +37,8 @@ end
 
 # LIB_DIR = '/home/runner/work/refactor-lich-5/refactor-lich-5/lib'
 
-require 'psms'
-require 'infomon'
+require 'gemstone/psms'
+require 'gemstone/infomon'
 
 module Char
   def self.name
