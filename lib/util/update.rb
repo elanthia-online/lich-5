47c21aa4f263881e18146b04f768cc9708a8f832
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-01-25 17:41:42 -0500

    [all][update.rb] bugfix: properly delete lib files on update

--- lib/util/update.rb
+++ lib/util/update.rb
@@ -208,7 +208,7 @@ module Lich
           FileUtils.mv(File.join(TEMP_DIR, new_target[0]), File.join(TEMP_DIR, filename))
 
           # delete all existing lib files to not leave old ones behind
-          FileUtils.rm_f(Dir.glob(File.join(LIB_DIR, "*")))
+          FileUtils.rm_rf(Dir.glob(File.join(LIB_DIR, "*")))
 
           _respond; _respond 'Copying updated lich files to their locations.'
 
@@ -248,7 +248,7 @@ module Lich
           _respond "No prior Lich5 version found. Seek assistance."
         else
           # delete all lib files
-          FileUtils.rm_f(Dir.glob(File.join(LIB_DIR, "*")))
+          FileUtils.rm_rf(Dir.glob(File.join(LIB_DIR, "*")))
           # copy all backed up lib files
           FileUtils.cp_r(File.join(restore_snapshot, "lib", "."), LIB_DIR)
           # delete array of core scripts
