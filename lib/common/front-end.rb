5dad9bcc006717205a773b88610003e79227b313
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-01-25 09:21:01 -0500

    add method to return current session_file_location

--- lib/common/front-end.rb
+++ lib/common/front-end.rb
@@ -8,18 +8,22 @@ module Lich
       @session_file = nil
       @tmp_session_dir = File.join Dir.tmpdir, "simutronics", "sessions"
 
-      def self.create_session_file(name, host, port)
+      def self.create_session_file(name, host, port, display_session: true)
         return if name.nil?
         FileUtils.mkdir_p @tmp_session_dir
         @session_file = File.join(@tmp_session_dir, "%s.session" % name.downcase.capitalize)
         session_descriptor = { name: name, host: host, port: port }.to_json
-        puts "writing session descriptor to %s\n%s" % [@session_file, session_descriptor]
+        puts "writing session descriptor to %s\n%s" % [@session_file, session_descriptor] if display_session
         File.open(@session_file, "w") do |fd|
           fd << session_descriptor
         end
       end
 
-      def self.cleanup_session_file()
+      def self.session_file_location
+        @session_file
+      end
+
+      def self.cleanup_session_file
         return if @session_file.nil?
         File.delete(@session_file) if File.exist? @session_file
       end
