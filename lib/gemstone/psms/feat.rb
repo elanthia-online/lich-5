25c3ff3cd51bcdcd1f2a3d1f76b9642755d3dfcc
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-03-13 12:01:40 -0400

    fix(feat): sidestep costs 10 stamina

--- lib/gemstone/psms/feat.rb
+++ lib/gemstone/psms/feat.rb
@@ -11,7 +11,7 @@ module Lich
          { long_name: 'covert_art_escape_artist',  short_name: 'escapeartist',       cost:  0 },
          { long_name: 'covert_art_keen_eye',       short_name: 'keeneye',            cost:  0 },
          { long_name: 'covert_art_poisoncraft',    short_name: 'poisoncraft',        cost:  0 },
-         { long_name: 'covert_art_sidestep',       short_name: 'sidestep',           cost:  0 },
+         { long_name: 'covert_art_sidestep',       short_name: 'sidestep',           cost: 10 },
          { long_name: 'covert_art_swift_recovery', short_name: 'swiftrecovery',      cost:  0 },
          { long_name: 'covert_art_throw_poison',   short_name: 'throwpoison',        cost:  0 },
          { long_name: 'covert_arts',               short_name: 'covert',             cost:  0 },
