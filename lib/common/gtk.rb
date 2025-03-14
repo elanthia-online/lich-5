89f5e5d4098591fdedff3fd6ac30d1e935f019f6
OSXLich-Doug <doug@play.net>
2025-03-14 16:31:15 -0500

    fix(all): retire gtk2 monkey patch for gtk3

--- lib/common/gtk.rb
+++ lib/common/gtk.rb
@@ -1,80 +1,6 @@
 module Lich
   module Common
     if defined?(Gtk)
-      Gdk.module_eval do
-        define_deprecated_singleton_method :screen_height, :warn => "Gdk::screen_height is deprecated; use monitor methods instead" do |_self|
-          99999
-        end
-
-        define_deprecated_singleton_method :screen_width, :warn => "Gdk::screen_width is deprecated; use monitor methods instead" do |_self|
-          99999
-        end
-      end
-
-      Gtk::Drag.module_eval do
-        define_deprecated_const :TARGET_SAME_APP, "Gtk::TargetFlags::SAME_APP"
-        define_deprecated_const :DEST_DEFAULT_ALL, "Gtk::DestDefaults::ALL"
-      end
-
-      Gtk.module_eval do
-        # Deprecation updates to keep gtk3 mostly going in gtk2
-        define_deprecated_const(:ComboBoxEntry, nil)
-        define_deprecated_const(:Tooltips, nil)
-
-        Gtk::ComboBox.class_eval do
-          def append_text(_text)
-            respond "'Gtk::ComboBox#append_text' is deprecated; use 'Gtk::ComboBoxText#append_text' instead"
-          end
-        end
-
-        Gtk::Entry.class_eval do
-          def set_text(text)
-            if text.nil?
-              respond "'Gtk::Entry#set_text' no longer accepts nil values; fix me"
-              text = ""
-            end
-            parent.set_text(text)
-            return self
-          end
-        end
-
-        Gtk::HBox.class_eval do
-          define_deprecated_singleton_method :new, :warn => "Use 'Gtk::Box.new(:horizontal, spacing)'." do |_self, homogeneous, spacing|
-            respond "'Gtk::Hbox' is deprecated; use 'Gtk::Box.new(:horizontal, spacing)'."
-            box = Gtk::Box.new(:horizontal, spacing)
-            box.set_homogeneous(homogeneous ? true : false)
-            box
-          end
-        end
-
-        Gtk::Notebook.class_eval do
-          def set_tab_border(_border)
-            respond "'Gtk::Notebook:set_tab_border()' is deprecated; fix me"
-            # noop
-            return self
-          end
-        end
-
-        Gtk::ToggleButton.class_eval do
-          def set_active(active)
-            if active.nil?
-              respond "'Gtk::ToggleButton#set_active' no longer accepts nil values; fix me"
-              active = false
-            end
-            parent.set_active(active)
-            return self
-          end
-        end
-
-        Gtk::VBox.class_eval do
-          define_deprecated_singleton_method :new, :warn => "Use 'Gtk::Box.new(:vertical, spacing)'." do |_self, homogeneous, spacing|
-            respond "'Gtk::VBox' is deprecated; use 'Gtk::Box.new(:vertical, spacing)' instead"
-            box = Gtk::Box.new(:vertical, spacing)
-            box.set_homogeneous(homogeneous ? true : false)
-            box
-          end
-        end
-
         # Calling Gtk API in a thread other than the main thread may cause random segfaults
         def Gtk.queue(&block)
           GLib::Timeout.add(1) {
@@ -120,9 +46,6 @@ module Lich
           }
         end
       end
-      #      def self.gtk_sleep_while_idle()
-      #        sleep 0.01
-      #      end
 
       unless File.exist?('logo.png')
         File.open('logo.png', 'wb') { |f|
@@ -226,9 +149,9 @@ module Lich
           @default_icon = GdkPixbuf::Pixbuf.new(:file => 'logo.png')
           # Add a function to call for when GTK is idle
           GLib::Idle.add do
-            # gtk_sleep_while_idle
             sleep 0.01
           end
+          @theme_state = Lich.track_dark_mode
         }
       rescue
         nil # fixme
