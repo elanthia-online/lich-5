8535f481f214f8c0e9b925a58f9db84f301006eb
OSXLich-Doug <doug@play.net>
2025-03-14 15:30:38 -0500

    downcase secondary wounds

--- lib/gemstone/critranks/impact_critical_table.rb
+++ lib/gemstone/critranks/impact_critical_table.rb
@@ -1309,7 +1309,7 @@ module Lich
                                   :silenced        => false,
                                   :slowed          => false,
                                   :wound_rank      => 3,
-                                  :secondary_wound => { :location => "NERVES", :wound_rank => 3 },
+                                  :secondary_wound => { :location => "nerves", :wound_rank => 3 },
                                   :regex           => /^Blow to back crushes spinal column./ },
                            9 =>
                                 { :type            => "impact",
@@ -1328,7 +1328,7 @@ module Lich
                                   :silenced        => false,
                                   :slowed          => false,
                                   :wound_rank      => 3,
-                                  :secondary_wound => { :location => "NERVES", :wound_rank => 3 },
+                                  :secondary_wound => { :location => "nerves", :wound_rank => 3 },
                                   :regex           => /^Blow to back removes the spinal column./ } },
           :right_arm  =>
                          { 0 =>
