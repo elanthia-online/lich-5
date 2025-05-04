4ece49fc823eb5fdddaa89499a9ca66a28afc3de
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-05-04 17:42:25 -0400

    Update game-loader.rb

--- lib/common/game-loader.rb
+++ lib/common/game-loader.rb
@@ -36,6 +36,8 @@ module Lich
         require File.join(LIB_DIR, 'gemstone', 'wounds.rb')
         require File.join(LIB_DIR, 'gemstone', 'scars.rb')
         require File.join(LIB_DIR, 'gemstone', 'gift.rb')
+        require File.join(LIB_DIR, 'gemstone', 'readylist.rb')
+        require File.join(LIB_DIR, 'gemstone', 'stowlist.rb')
         ActiveSpell.watch!
         self.common_after
       end
