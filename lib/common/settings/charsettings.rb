fc4fa4c0119ae28e725c5bf84a0c630e7ac18bb2
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-05-11 12:16:21 -0400

    fix: send to Settings.[]

--- lib/common/settings/charsettings.rb
+++ lib/common/settings/charsettings.rb
@@ -5,7 +5,7 @@ module Lich
   module Common
     module CharSettings
       def CharSettings.[](name)
-        Settings.to_hash("#{XMLData.game}:#{XMLData.name}")[name]
+        Settings.[]("#{XMLData.game}:#{XMLData.name}", name)
       end
 
       def CharSettings.[]=(name, value)
