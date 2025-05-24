e9304e01972616173699150698499fb024bff82a
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-05-24 11:02:23 -0400

    fix: convert type values from string to symbol

--- lib/gemstone/psms/feat.rb
+++ lib/gemstone/psms/feat.rb
@@ -24,7 +24,7 @@ module Lich
       @@feats = {
         "absorb_magic"              => {
           :short_name => "absorbmagic",
-          :type       => "buff",
+          :type       => :buff,
           :cost       => 0,
           :regex      => Regexp.union(/You open yourself to the ravenous void at the core of your being, allowing it to surface\.  Muted veins of metallic grey ripple just beneath your skin\./,
                                       /You strain, but the void within remains stubbornly out of reach\.  You need more time\./),
@@ -32,28 +32,28 @@ module Lich
         },
         "chain_armor_proficiency"   => {
           :short_name => "chainarmor",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Chain Armor Proficiency is passive and cannot be activated\./,
           :usage      => nil
         },
         "chastise"                  => {
           :short_name => "chastise",
-          :type       => "attack",
+          :type       => :attack,
           :cost       => 10,
           :regex      => /as you lunge at .+? in a quick and vicious strike!$/,
           :usage      => "chastise"
         },
         "combat_mastery"            => {
           :short_name => "combatmastery",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Combat Mastery is passive and cannot be activated\./,
           :usage      => nil
         },
         "covert_art_escape_artist"  => {
           :short_name => "escapeartist",
-          :type       => "reaction",
+          :type       => :reaction,
           :cost       => 0,
           :regex      => Regexp.union(/You roll your shoulders and subtly test the flexion of your joints, staying limber for ready escapes\./,
                                       /You were unable to find any targets that meet Covert Art\: Escape Artist's reaction requirements\./),
@@ -61,35 +61,35 @@ module Lich
         },
         "covert_art_keen_eye"       => {
           :short_name => "keeneye",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Covert Art\: Keen Eye is always active as long as you stay up to date on training\./,
           :usage      => nil
         },
         "covert_art_poisoncraft"    => {
           :short_name => "poisoncraft",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /USAGE\: FEAT POISONCRAFT \{options\} \[args\]/,
           :usage      => nil
         },
         "covert_art_sidestep"       => {
           :short_name => "sidestep",
-          :type       => "buff",
+          :type       => :buff,
           :cost       => 10,
           :regex      => /You tread lightly and keep your head on a swivel, prepared to sidestep any loose salvos that might stray your way\./,
           :usage      => "sidestep"
         },
         "covert_art_swift_recovery" => {
           :short_name => "swiftrecovery",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Covert Art\: Swift Recovery is always active as long as you stay up to date on training\./,
           :usage      => nil
         },
         "covert_art_throw_poison"   => {
           :short_name => "throwpoison",
-          :type       => "attack",
+          :type       => :attack,
           :cost       => 15,
           :regex      => Regexp.union(/What did the .+ ever do to you\?/,
                                       /You pop the cork on .+ and, with a nimble flick of the wrist, fling a portion of its contents in a wide arc\!/,
@@ -98,21 +98,21 @@ module Lich
         },
         "covert_arts"               => {
           :short_name => "covert",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /USAGE\: FEAT COVERT \{options\} \[args\]/,
           :usage      => nil
         },
         "critical_counter"          => {
           :short_name => "criticalcounter",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Critical Counter is passive and cannot be activated\./,
           :usage      => nil
         },
         "dispel_magic"              => {
           :short_name => "dispelmagic",
-          :type       => "buff",
+          :type       => :buff,
           :cost       => 30,
           :regex      => Regexp.union(/You reach for the emptiness within, but there are no spells afflicting you to dispel\./,
                                       /You reach for the emptiness within\.  A single, hollow note reverberates through your core, resonating outward and scouring away the energies that cling to you\./),
@@ -120,14 +120,14 @@ module Lich
         },
         "dragonscale_skin"          => {
           :short_name => "dragonscaleskin",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /The Dragonscale Skin\s{1,2}feat is always active once you have learned it\./,
           :usage      => nil
         },
         "guard"                     => {
           :short_name => "guard",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => Regexp.union(/Guard what\?/,
                                       /You move over to .+ and prepare to guard [a-z]+ from attack\./,
@@ -139,70 +139,70 @@ module Lich
         },
         "kroderine_soul"            => {
           :short_name => "kroderinesoul",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Kroderine Soul is passive and always active as long as you do not learn any spells\.  You have access to two new abilities\:/,
           :usage      => nil
         },
         "light_armor_proficiency"   => {
           :short_name => "lightarmor",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Light Armor Proficiency is passive and cannot be activated\./,
           :usage      => nil
         },
         "martial_arts_mastery"      => {
           :short_name => "martialarts",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Martial Arts Mastery is passive and cannot be activated\./,
           :usage      => nil
         },
         "martial_mastery"           => {
           :short_name => "martialmastery",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Martial Mastery is passive and cannot be activated\./,
           :usage      => nil
         },
         "mental_acuity"             => {
           :short_name => "mentalacuity",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /The Mental Acuity\s{1,2}feat is always active once you have learned it\./,
           :usage      => nil
         },
         "mystic_strike"             => {
           :short_name => "mysticstrike",
-          :type       => "buff",
+          :type       => :buff,
           :cost       => 10,
           :regex      => /You prepare yourself to deliver a Mystic Strike with your next attack\./,
           :usage      => "mysticstrike"
         },
         "mystic_tattoo"             => {
           :short_name => "tattoo",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Usage\:/,
           :usage      => nil
         },
         "perfect_self"              => {
           :short_name => "perfectself",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /The Perfect Self feat is always active once you have learned it\.  It provides a constant enhancive bonus to all your stats\./,
           :usage      => nil
         },
         "plate_armor_proficiency"   => {
           :short_name => "platearmor",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Plate Armor Proficiency is passive and cannot be activated\./,
           :usage      => nil
         },
         "protect"                   => {
           :short_name => "protect",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => Regexp.union(/Protect what\?/,
                                       /You move over to .+ and prepare to protect [a-z]+ from attack\./,
@@ -214,21 +214,21 @@ module Lich
         },
         "scale_armor_proficiency"   => {
           :short_name => "scalearmor",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /Scale Armor Proficiency is passive and cannot be activated\./,
           :usage      => nil
         },
         "shadow_dance"              => {
           :short_name => "shadowdance",
-          :type       => "buff",
+          :type       => :buff,
           :cost       => 30,
           :regex      => /You focus your mind and body on the shadows\./,
           :usage      => nil
         },
         "silent_strike"             => {
           :short_name => "silentstrike",
-          :type       => "attack",
+          :type       => :attack,
           :cost       => 20,
           :regex      => Regexp.union(/Silent Strike can not be used with fire as the attack type\./,
                                       /You quickly leap from hiding to attack\!/),
@@ -236,28 +236,28 @@ module Lich
         },
         "vanish"                    => {
           :short_name => "vanish",
-          :type       => "buff",
+          :type       => :buff,
           :cost       => 30,
           :regex      => /With subtlety and speed, you aim to clandestinely vanish into the shadows\./,
           :usage      => "vanish"
         },
         "weapon_bonding"            => {
           :short_name => "weaponbonding",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /USAGE\:/,
           :usage      => nil
         },
         "weighting"                 => {
           :short_name => "wps",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /USAGE\: FEAT WPS \{options\} \[args\]/,
           :usage      => nil
         },
         "padding"                   => {
           :short_name => "wps",
-          :type       => "passive",
+          :type       => :passive,
           :cost       => 0,
           :regex      => /USAGE\: FEAT WPS \{options\} \[args\]/,
           :usage      => nil
