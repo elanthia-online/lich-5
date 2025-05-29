1a98bbbbb84739cddd5536a65d676b0fcbe40b1d
MahtraDR <93822896+MahtraDR@users.noreply.github.com>
2025-05-29 18:27:12 +1200

    fix(dr): Release Khri Vanish if Thief, when going visible

--- lib/dragonrealms/commons/common.rb
+++ lib/dragonrealms/commons/common.rb
@@ -478,6 +478,7 @@ module Lich
 
         # handle khri silence as it's not part of base-spells data, and method of ending it differs from spells
         bput('khri stop silence', 'You attempt to relax') if DRSpells.active_spells.keys.include?('Khri Silence')
+        bput('khri stop vanish', /^You would need to start Vanish/, /^Your control over the limited subversion of reality falters/) if DRStats.guild == "Thief"
       end
 
       def check_encumbrance(refresh = true)
