12e8e9c9f43b70864984e7fe0780d86728537871
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-11-24 14:57:13 -0500

    fix: rubocop

--- lib/common/game-loader.rb
+++ lib/common/game-loader.rb
@@ -33,7 +33,7 @@ module Lich
         # require File.join(LIB_DIR, 'gemstone', 'character', 'disk.rb') # dup
         require File.join(LIB_DIR, 'gemstone', 'group.rb')
         require File.join(LIB_DIR, 'gemstone', 'critranks')
-        require File.join(LIB_DIR, 'gemstone', 'injured.rb')
+        require File.join(LIB_DIR, 'gemstone', 'injured')
         require File.join(LIB_DIR, 'gemstone', 'wounds.rb')
         require File.join(LIB_DIR, 'gemstone', 'scars.rb')
         require File.join(LIB_DIR, 'gemstone', 'gift.rb')
