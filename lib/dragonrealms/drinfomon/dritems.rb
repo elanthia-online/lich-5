da8e8356cfde95d9e08dccfbf6e08f24750af33b
MahtraDR <93822896+MahtraDR@users.noreply.github.com>
2025-12-03 07:00:48 +1300

    fix dritems.rb

--- lib/dragonrealms/drinfomon/dritems.rb
+++ lib/dragonrealms/drinfomon/dritems.rb
@@ -11,7 +11,6 @@ module Lich
       end
       
       def self.list
-        Lich.log("DRItems.list: current list='#{@@list.inspect}'")
         @@list
       end
 
@@ -20,14 +19,11 @@ module Lich
       end
 
       def self.update_item(item_string, cmd, full_description)
-        # Lich.log("DRItems.update_item: item='#{item_string}', cmd='#{cmd}', full_description='#{full_description}'")
         item = Hash.new
         item[:name] = item_string
         item[:cmd] = cmd
         item[:full_description] = full_description
-        # Lich.log("DRItems.update_item: storing item='#{item.inspect}'")
         @@list << item
-        Lich.log("DRItems.update_item: current list='#{@@list.inspect}'")
       end
     end
   end
