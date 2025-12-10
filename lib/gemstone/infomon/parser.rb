f507a0b6a6e76797764ca06d2a0470ff41bd22e4
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2025-12-09 20:34:52 -0500

    Update lib/gemstone/infomon/parser.rb
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- lib/gemstone/infomon/parser.rb
+++ lib/gemstone/infomon/parser.rb
@@ -395,7 +395,7 @@ module Lich
               Infomon.set('resources.shadow_essence', (Lich::Resources.shadow_essence.to_i - (match[:amount].to_i / 60 - 1)))
               :ok
             when Pattern::SacrificeChannel, Pattern::SacrificeInfest, Pattern::SacrificeFate, Pattern::SacrificeShift
-              Infomon.set('resources.shadow_essence', (Lich::Resrouces.shadow_essence.to_i - 1))
+              Infomon.set('resources.shadow_essence', (Lich::Resources.shadow_essence.to_i - 1))
               :ok
             when Pattern::GigasArtifactFragments
               match = Regexp.last_match
