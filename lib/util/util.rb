17701608738fc2be5feae39e7fc55f4361d88f8c
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2026-01-14 17:36:26 -0500

    fix: update debug messaging

--- lib/util/util.rb
+++ lib/util/util.rb
@@ -250,20 +250,25 @@ module Lich
         begin
           unless installed_gems.include?(gem_name)
             respond("--- Lich: Installing missing ruby gem '#{gem_name}' now, please wait!")
+            Lich.log("--- Lich: Installing missing ruby gem '#{gem_name}' now, please wait!")
             result = installer.install(gem_name)
+            Gem.clear_paths
             Lich.log("RubyGem Installer Result: #{result.inspect}")
-            respond("--- Lich: Done installing '#{gem_name}' gem!")
           end
           Gem.clear_paths
           unless Gem::Specification.map { |gem| gem.name }.sort.uniq.include?(gem_name)
+            Lich.log("RubyGems failed, attempting system method instead!")
             result = system(File.join(RbConfig::CONFIG['bindir'], 'gem'), 'install', gem_name)
             Lich.log("SYSTEM Call Result: #{result.inspect}")
             Gem.clear_paths
           end
+          respond("--- Lich: Done installing '#{gem_name}' gem!")
+          Lich.log("--- Lich: Done installing '#{gem_name}' gem!")
           require gem_name if should_require
         rescue LoadError, StandardError
           respond("--- Lich: error: Failed to install/require Ruby gem: #{gem_name}")
           respond("--- Lich: error: #{$!}")
+          Lich.log("installed_gems.include?(#{gem_name}): #{installed_gems.include?(gem_name)} - #{installed_gems.find_all { |gem| gem == gem_name }.inspect}")
           Lich.log("error: Failed to install/require Ruby gem: #{gem_name}")
           Lich.log("error: #{$!}")
           failed_gems.push(gem_name)
