13c08fb969e91311411d0892c1ad63d6e1e25bb8
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2026-04-23 10:07:44 -0400

    fix(all): safeguard gtk logo.png loading
    fixes:
    ```
    Failed to open file “logo.png”: No such file or directory
    ```
    When using various other launch methods like Warlock/Custom launch.

--- lib/common/gtk.rb
+++ lib/common/gtk.rb
@@ -47,8 +47,8 @@ module Lich
       end
     end
 
-    unless File.exist?('logo.png')
-      File.open('logo.png', 'wb') { |f|
+    unless File.exist?(File.join(LICH_DIR, 'logo.png'))
+      File.open(File.join(LICH_DIR, 'logo.png'), 'wb') { |f|
         f.write '
       iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAA
       CqaXHeAAAPrUlEQVR4Ae1bDVRT5xl+t7azXY/b2rOu7ep2zuaZm5unbqX7s7
@@ -146,7 +146,7 @@ module Lich
 
     begin
       Gtk.queue {
-        @default_icon = GdkPixbuf::Pixbuf.new(:file => 'logo.png')
+        @default_icon = GdkPixbuf::Pixbuf.new(:file => File.join(LICH_DIR, 'logo.png'))
         # Add a function to call for when GTK is idle
         GLib::Idle.add do
           sleep 0.01
